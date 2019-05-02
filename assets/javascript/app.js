38aac38cca3a1fc6eabe33e4fbe5a846610b4aeb
Keanu Korkor <keanukorkor@Keanus-MacBook-Pro.local>
2019-05-02 14:33:16 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,34 @@
-
-var topics = ["Real Madrid","Barcelona","Chelsea","Juventus","Liverpool","Arsenal","Manchester United","Manchester City","Bayern Muinch","DC United",
- "PSG","AC Milan"];
-var gif; 
-var pausedGif; 
-var stillGif;
-var animatedGif; 
+var apiKey = "&api_key=pwsWqVJCSnqvzxAsVlL7boiSI19zv0oM"
+var queryurl = "https://api.giphy.com/v1/gifs/search?" + apiKey;
+var socteams;
+var defaultteams = ["real madrid", "Barcelona", "chelsea fc", "Manchester United"]
+var searchString;
 
 
-function createButtons(){
-	$('#stButtons').empty();
-	for(var i = 0; i < topics.length; i++) {
-		var teamBtn = $('<button>').text(topics[i]).addClass('teamBtn').attr('data-name', topics[i]);
-		$('#stButtons').append(teamBtn);
+// looping between the buttons and creating a button
+function creatbuttons() {
+	for (var i = 0; i < defaultteams.length; i++) {
+		var defaultbutton = $("<button class='addedbtn' data-name =' " + defaultteams[i] + " '>" + defaultteams[i] + "</button>")
+		$(".displaybtn").append(defaultbutton)
 	}
 }
+creatbuttons()
 
-$(document).on('click', '.teamBtn', function(){
-		$('.display').empty();
+$(document).on("click", ".addedbtn", function () {
+	var nospaces= $(this).data("name").replace(/\s/g, "+")
+	searchString = "&q=" + $(this).data("name").split(" ").join("+")
+	console.log(nospaces)
+	$.ajax({
+		url: queryurl + searchString + "&limit=10",
+		method: "GET"
 
-		var socteams = $(this).attr('data-name');
-		
-		var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + socteams + "&api_key=pwsWqVJCSnqvzxAsVlL7boiSI19zv0oM&limit=10";
-		$.ajax({url: giphyURL, method: 'GET'}).done(function(giphy){
-			gif = giphy.data;
-			console.log(gif)
-			for
-				$.each(gif, function(_index,value){
-				animatedGif= value.images.original.url;
-				pausedGif = value.images.original_still.url;
-				var gifRating = value.rating;
-
-				if (gifRating == ''){
-					gifRating = 'unrated';
-				}
-				var rating = $('<h5>').html('Rated: '+ gifRating).addClass('ratingStyle');
-				stillGif= $('<img>').attr('data-animated', animatedGif).attr('data-paused', pausedGif).attr('src', pausedGif).addClass('playOnHover');
-				var fullGifDisplay = $('<button>').append(rating, stillGif);
-				$('.display').append(fullGifDisplay);
-			});
-		});
-	});
-
-createButtons()
+	}).then(function (giphy) {
+		console.log("query url: " + queryurl + searchString)
+		//This is the source
+		console.log(giphy.data[0])
+		for (var i = 0; i < giphy.data.length; i++) {
+			var gif = $("<img>").attr("src", giphy.data[i].images.downsized.url);
+			$(".displayGif").prepend(gif)
+		}
+	})
+})
